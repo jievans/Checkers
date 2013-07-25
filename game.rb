40c4e14df8071f83cfd6cf27224f3ed5b0674ca4
App Academy Student <student@appacademy.io>
2013-07-25 13:06:51 -0700

    Added build board method

--- game.rb
+++ game.rb
@@ -18,6 +18,38 @@ class Board
     @grid[row][col]
   end
 
+  def build_board
+    @grid[7][0] = Piece.new(:white, [7, 0])
+    @grid[7][2] = Piece.new(:white, [7, 2])
+    @grid[7][4] = Piece.new(:white, [7, 4])
+    @grid[7][6] = Piece.new(:white, [7, 6])
+
+    @grid[5][0] = Piece.new(:white, [5, 0])
+    @grid[5][2] = Piece.new(:white, [5, 2])
+    @grid[5][4] = Piece.new(:white, [5, 4])
+    @grid[5][6] = Piece.new(:white, [5, 6])
+
+    @grid[6][1] = Piece.new(:white, [6, 1])
+    @grid[6][3] = Piece.new(:white, [6, 3])
+    @grid[6][5] = Piece.new(:white, [6, 5])
+    @grid[6][7] = Piece.new(:white, [6, 7])
+
+    @grid[0][1] = Piece.new(:red, [0, 1])
+    @grid[0][3] = Piece.new(:red, [0, 3])
+    @grid[0][5] = Piece.new(:red, [0, 5])
+    @grid[0][7] = Piece.new(:red, [0, 7])
+
+    @grid[2][1] = Piece.new(:red, [2, 1])
+    @grid[2][3] = Piece.new(:red, [2, 3])
+    @grid[2][5] = Piece.new(:red, [2, 5])
+    @grid[2][7] = Piece.new(:red, [2, 7])
+
+    @grid[1][0] = Piece.new(:red, [1, 0])
+    @grid[1][2] = Piece.new(:red, [1, 2])
+    @grid[1][4] = Piece.new(:red, [1, 4])
+    @grid[1][6] = Piece.new(:red, [1, 6])
+  end
+
   def set_piece(piece, position)
     piece.position = position
     row, col = position
@@ -75,14 +107,23 @@ class Board
   end
 
   def perform_moves!(move_sequence)
-
     if at_position(move_sequence.first).nil?
       raise InvalidMoveError, "You haven't selected a piece to start moving."
     end
 
-    until move_sequence.empty?
+    until move_sequence.count == 1
+      first_pos = move_sequence[0]
+      second_pos = move_sequence[1]
 
+      is_jump = (first_pos.first - second_pos.first).abs > 1 ? true: false
 
+      if is_jump
+        perform_jump(first_pos, second_pos)
+      else
+        perform_slide(first_pos, second_pos)
+      end
+
+      move_sequence.shift
     end
   end
 
